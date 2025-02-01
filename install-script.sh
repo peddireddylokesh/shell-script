67098cd09536a019599c56adcd0e9ba65c214ff4
peddireddylokesh <peddireddylokesh999@gmail.com>
2025-02-01 20:15:51 +0530

    username added

--- install-script.sh
+++ install-script.sh
@@ -3,5 +3,7 @@
 USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
     echo "Error you must have sudo access to execute this script"
+elif [ $USERID -eq 0 ]; then
+    echo "You have sudo access"
+    dnf install mysql -y
 fi
-dnf install mysql -y
