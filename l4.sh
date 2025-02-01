f12c8e4bd75e228bb72f861cd069ba8c54482092
peddireddylokesh <peddireddylokesh999@gmail.com>
2025-02-01 16:13:52 +0530

    username added

--- l4.sh
+++ l4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "please enter your user name" # text entered here will be added as a value or variable
-read USERNAME # it is a variable name
+read -s USERNAME # it is a variable name
 echo "username is $USERNAME"
 echo "please enter your password"
-read PASSWORD  
+read -s PASSWORD  #if we add -s before username to reading format like [ read -s USERNAME ] no one can see the typing or cursor
