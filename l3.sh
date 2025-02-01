1b6e3f7302cd9568be23a46e55ede4df61968038
peddireddylokesh <peddireddylokesh999@gmail.com>
2025-02-01 14:23:37 +0530

    removed files

--- l3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#its a commented line
-echo "its a 3rd file"
-echo "haha lol"
-echo "omg"
-echo "lol"
-echo "how are you?"
-echo "im fine"
--- lokesh.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# it's a commented line
-echo "Hello, Lokesh"
-echo "Today is $(date)"
