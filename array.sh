033baf8684db01000ce9e9dd7720e371ce769899
peddireddylokesh <peddireddylokesh999@gmail.com>
2025-02-01 17:23:23 +0530

    username added

--- array.sh
+++ array.sh
@@ -3,8 +3,8 @@
 
 MOVIES=("Pushpa" "rrr" "salar")
 
-echo "first movie is : ${MOVIES[0]}"
+echo "first movie is  : ${MOVIES[0]}"
 echo "second movie is : ${MOVIES[1]}"
-echo "third movie is : ${MOVIES[2]}"
+echo "third movie is  : ${MOVIES[2]}"
 
-echo "all movies are : ${MOVIES[@]}"
+echo "All movies are  : ${MOVIES[@]}"
---
+++ special-variable.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "all variables passed: $@"
+echo "number of variables passed: $#"
+echo "script name: $0"
+echo "present working directory: $PWD"
+echo "last command status: $?"
+echo "home directory of current user: $HOME"
+echo "current user: $USER"
+echo "process id of current shell-script: $$"
+sleep 10 &
+echo "last background process id: $!"
