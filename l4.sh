588870187e4403b7c38f04c85162a469f98a4cba
peddireddylokesh <peddireddylokesh999@gmail.com>
2025-02-01 16:08:07 +0530

    username added

--- l4.sh
+++ l4.sh
@@ -2,3 +2,5 @@
 echo "please enter your user name" # text entered here will be added as a value or variable
 read USERNAME # it is a variable name
 echo "username is $USERNAME"
+echo "please enter your password"
+read PASSWORD  
