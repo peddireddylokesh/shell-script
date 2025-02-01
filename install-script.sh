0bdacc518d9b4e36da05e0fabb2782e7e1fbefae
peddireddylokesh <peddireddylokesh999@gmail.com>
2025-02-01 21:01:18 +0530

    username added

--- install-script.sh
+++ install-script.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
+userid=$(id -u)
+#     read -p "Do you want to install mysql [y/n] : " input
 
-USERID=$(id -u)
-    read -p "Do you want to install mysql [y/n] : " input
-
-if [ $input == "y" ]; then
+# if [ $input == "y" ]; then
    
-    if [ $USERID -ne 0 ]; then
-        echo "Error you must have sudo access to execute this script"
-    else
-        echo "You have sudo access, installing mysql"
-        dnf install mysql -y
-    fi
-elif [ $input == "n" ]; then
-    if [ $USERID -ne 0 ]; then
-        echo "You must have sudo access to uninstall mysql"
-    else
-        echo "You have sudo access, uninstalling mysql"
-        sudo dnf remove mysql -y
-    fi
-else
-    echo "Invalid input"
-fi
+#     if [ $USERID -ne 0 ]; then
+#         echo "Error:: you must have sudo access to execute this script"
+#     else
+#         echo "You have sudo access, installing mysql"
+#         dnf install mysql -y
+#     fi
+# elif [ $input == "n" ]; then
+#     if [ $USERID -ne 0 ]; then
+#         echo "You must have sudo access to uninstall mysql"
+#     else
+#         echo "You have sudo access, uninstalling mysql"
+#         sudo dnf remove mysql -y
+#     fi
+# else
+#     echo "Invalid input"
+# fi
 
+if [ $userid -ne 0 ];then
+    echo "Error:: you must have sudo access to execute this script"
+fi
+dnf list installed mysql
+if [ $? -eq 0 ];then
+    echo "You have sudo access, installing mysql"
+    dnf install mysql -y
+fi
