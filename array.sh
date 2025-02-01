bb0ae76171523421eb999c1735eb48a6761a6303
peddireddylokesh <peddireddylokesh999@gmail.com>
2025-02-01 17:03:23 +0530

    username added

---
+++ array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+    #array declaration
+MOVIES=("Pushpa” “rrr” “salar”)
+
+echo "first movie is : ${MOVIES[0]}"
+echo "second movie is : ${MOVIES[1]}"
+echo "third movie is : ${MOVIES[2]}"
+
+echo "all movies are : ${MOVIES[@]}"
