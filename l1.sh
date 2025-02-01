4bd4fc4f44e37b34e7064af8c852bc2ce56871b5
peddireddylokesh <peddireddylokesh999@gmail.com>
2025-02-01 14:18:56 +0530

    upadating both files

--- l1.sh
+++ l1.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-echo "Hello World the above script is saved in a file named  l1.sh . To run the script, you need to make it executable. "
+echo "Hello World the above script is saved in a file named  l2.sh .
+ To run the script, you need to make it executable. "
+echo "To make the script executable, you need to run the following command:"
