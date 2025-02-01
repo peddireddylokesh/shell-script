518bd744f0ec4614951a7295f2aefea3cee5af8e
peddireddylokesh <peddireddylokesh999@gmail.com>
2025-02-01 15:10:56 +0530

    renamed t3.sh to variable.sh

--- t3.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "oh this is third file "
-echo "hi im learning gut hub"
-echo "i have to learn more"
---
+++ variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "oh this is third file "
+echo "hi im learning gut hub"
+echo "i have to learn more"
+
+echo "loki::hi thor"
+echo "thor::hello loki"
+echo "loki::how are you doing"
+echo "thor::i am doing good"
+echo "loki::i am also doing good"
+echo "thor::i am happy to hear that"
