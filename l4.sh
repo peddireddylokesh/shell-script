f7518b889b0955bbee4050853f1232c1e93b9c5e
peddireddylokesh <peddireddylokesh999@gmail.com>
2025-02-01 16:04:45 +0530

    extra co added

--- l4.sh
+++ l4.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-echo "please enter your user name"
-read USERNAME
+echo "please enter your user name" # text entered here will be added as a value or variable
+read USERNAME # it is a variable name
+echo "username is $USERNAME"
