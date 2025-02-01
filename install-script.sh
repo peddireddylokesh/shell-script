cbecdfd54925f280634f54ce239bd4c0d7af3e71
peddireddylokesh <peddireddylokesh999@gmail.com>
2025-02-01 20:36:27 +0530

    username added

--- install-script.sh
+++ install-script.sh
@@ -3,15 +3,27 @@
 USERID=$(id -u)
     read -p "Do you want to install mysql [y/n] : " input
 
-if [ $USERID -ne 0 ]; then
-    echo "Error you must have sudo access to execute this script"
-elif [ $USERID -eq 0 ]; then
-    echo "You have sudo access"
-    dnf install mysql -y
-elif [ $USERID -eq 1 ]; then
-    echo "you have entered 1 so im uninstalling mysql with dependencies too"
-    sudo dnf remove mysql-server -y
-    sudo dnf autoremove -y
-    sudo systemctl status mysqld
-    rpm -qa | grep mysql
+if [ $input == "y" ]; then
+   
+    if [ $USERID -ne 0 ]; then
+        echo "Error you must have sudo access to execute this script"
+    else
+        echo "You have sudo access, installing mysql"
+        dnf install mysql -y
+    fi
+elif [ $input == "n" ]; then
+    if [ $USERID -ne 0 ]; then
+        echo "You must have sudo access to uninstall mysql"
+    else
+        echo "You have sudo access, uninstalling mysql"
+        sudo dnf remove mysql-server -y
+        sudo dnf autoremove -y
+        sudo systemctl stop mysqld
+        sudo systemctl disable mysqld
+        sudo systemctl status mysqld
+        rpm -qa | grep mysql
+    fi
+else
+    echo "Invalid input"
 fi
+
