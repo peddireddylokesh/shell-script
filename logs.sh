eefaa63ec306d2845e8356c0ba209165a71d92a6
peddireddylokesh <peddireddylokesh999@gmail.com>
2025-02-02 14:32:52 +0530

    username added

--- logs.sh
+++ logs.sh
@@ -7,16 +7,17 @@ userid=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
+N="\e[0m"
 Log_folder="/var/log/shellscript-logs"
 logfile=$(echo $0 | cut -d "." -f 1)
 timestamp=$(date '+%d-%m-%Y-%H-%M-%S')
 logfilename="$Log_folder/$logfile-$timestamp.log"
 validate(){
       if [ $1 -eq 0 ];then
-        echo -e "$2 ...$G successfully"
+        echo -e "$2 ...$G successfully $N"
         exit 1
     else
-        echo -e "Error:: $2  .....$R failed"
+        echo -e "Error:: $2  .....$R failed $N"
     fi
 }
 
@@ -33,7 +34,7 @@ if [ $? -ne 0 ];then #not installed
     dnf install mysql -y  &>>$logfilename
     validate $? "installing mysql"
 else
-    echo -e "mysql is already..... $Y INSTALLED"
+    echo -e "mysql is already..... $Y INSTALLED $N"
 fi
 
 
@@ -44,5 +45,5 @@ if [ $? -ne 0 ];then #not installed
     dnf install git -y  &>>$logfilename
      validate $? "installing git"
 else
-    echo -e "git is already..... $Y INSTALLED"
+    echo -e "git is already..... $Y INSTALLED $N"
 fi
