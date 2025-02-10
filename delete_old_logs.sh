10574c7d30925cd4e1bc3a65c5c048c5d975c48d
peddireddylokesh <peddireddylokesh999@gmail.com>
2025-02-10 16:19:57 +0530

    testing shellscript

--- delete_old_logs.sh
+++ delete_old_logs.sh
@@ -30,3 +30,8 @@ echo "script started executing at $timestamp" &>>$logfilename
 
 files_to_delete=$(find $source_dir -name "*.log" -mtime +7)
 echo "files to be deleted: $files_to_delete"
+
+while read -r file_to_do
+do
+    echo "deleting $file_to_do"
+done <<< $files_to_delete
