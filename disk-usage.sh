6b6e48129b976f68cd1507b34b04fdfe415ff7cc
peddireddylokesh <peddireddylokesh999@gmail.com>
2025-02-13 23:39:46 +0530

    testing shellscript

--- disk-usage.sh
+++ disk-usage.sh
@@ -5,7 +5,10 @@ disk_threshhold=5 #in real projects it would
 while read -r line
 do 
    
-    usage=$(echo $line | awk  -F " " '{print $6F}')
+    usage=$(echo $line | awk  -F " " '{print $6F}' | cut -d "%" -f1)
     partition=$(echo $line | awk  -F " " '{print $6F}')
     echo "partition  is $partition ,usage : $usage"
 done <<< $disk_usage
+
+
+
