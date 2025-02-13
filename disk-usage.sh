41fa14cd2cbfcc7d53b7e829b6b5baa5dbe831b8
peddireddylokesh <peddireddylokesh999@gmail.com>
2025-02-13 22:55:01 +0530

    testing shellscript

--- disk-usage.sh
+++ disk-usage.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 disk_usage=$(df -hT | grep xfs)
+disk_threshhold=5 #in real projects it would
 while read -r line
 do 
+   
     echo "$line"
-done <<<$disk_usage
+    usage=$(echo $line | awk  -F " "'{print $6F}')
+    partition=$(echo $line| awk  -F " "'{print $6F}'j
+    echo "partition  is $partition" $usage 
+jone <<<$disk_usage
