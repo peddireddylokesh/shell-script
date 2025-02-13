eabba4dd8a709e3a2210e5df61fc49da289d83a4
peddireddylokesh <peddireddylokesh999@gmail.com>
2025-02-14 00:05:19 +0530

    testing shellscript

--- disk-usage.sh
+++ disk-usage.sh
@@ -8,6 +8,10 @@ do
     usage=$(echo $line | awk  -F " " '{print $6F}' | cut -d "%" -f1)
     partition=$(echo $line | awk  -F " " '{print $6F}')
     echo "partition  is $partition ,usage : $usage"
+    if [$usage -gt $disk_threshhold]
+    then
+        echo "Disk usage is more than $disk_threshhold"
+    fi
 done <<< $disk_usage
 
 
