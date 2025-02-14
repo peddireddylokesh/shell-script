70b8ea6f69153b134318dfac0ed363d30c7ba654
peddireddylokesh <peddireddylokesh999@gmail.com>
2025-02-14 14:18:13 +0530

    testing shellscript

--- script1.sh
+++ script1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 msg="script1"
-greet="hello from script 1"
+greet="hello i'm script 1"
 
-
+source ./script2.sh
 echo "hello from : $msg"
 
 sh script2.sh
--- script2.sh
+++ script2.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 msg="script2"
 echo "hello from : $msg"
-
-echo "$greet"
+echo "greet message is from script1 $greet"
