af49907c61470dfa31f6bff4a10b2d7dd7e87bcf
peddireddylokesh <peddireddylokesh999@gmail.com>
2025-02-14 15:53:23 +0530

    testing shellscript

--- sourcetest.sh
+++ sourcetest.sh
@@ -6,6 +6,7 @@ source_dir="/home/ec2-user/app-logs"
 echo "script started executing at $timestamp" &>>$logfilename
 
 files_to_delete=$(find $source_dir -name "*.log" -mtime +7)
+cat $files_to_delete
 echo "files to be deleted: $files_to_delete"
 
 while read -r file_to_do
