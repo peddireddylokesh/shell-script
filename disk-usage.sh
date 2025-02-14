1bf0c962910ba26ad36c8ef5a21b73a673af700a
peddireddylokesh <peddireddylokesh999@gmail.com>
2025-02-14 10:35:59 +0530

    testing shellscript

--- disk-usage.sh
+++ disk-usage.sh
@@ -2,21 +2,21 @@
 
 disk_usage=$(df -hT | grep xfs) # Extract only xfs filesystem lines
 disk_threshhold=5  # Define threshold (in percentage)
-
 msg=""
+
 while read -r line
 do 
    
     usage=$(echo $line | awk  -F " " '{print $6F}' | cut -d "%" -f1)
-    partition=$(echo $line | awk  -F " " '{print $6F}')
+    partition=$(echo $line | awk  -F " " '{print $NF}')
    # echo "partition  is $partition ,usage : $usage"
     if [ $usage -ge $disk_threshhold ]
     then
-        msg+="Disk usage is high  $partition usage is $usage \n "
+        msg+="Disk usage is high --- $partition usage is --- $usage \n "
     fi
 done <<< $disk_usage
 
-echo -e "message $msg"
+echo -e "message :: $msg"
 
 echo "$msg" | sendmail -s "high disk usage" peddireddylokesh999@gmail.com
 
