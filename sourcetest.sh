826415f061ae94b8c0124639fb0b8b8048f71366
peddireddylokesh <peddireddylokesh999@gmail.com>
2025-02-14 15:59:36 +0530

    testing shellscript

--- sourcetest.sh
+++ sourcetest.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
+
 source ./common.sh
 
 source_dir="/home/ec2-user/app-logs"
+logfilename="/var/log/script.log"  # Define your log file
 
-echo "script started executing at $timestamp" &>>$logfilename
+echo "Script started executing at $(date)" &>> "$logfilename"
 
-files_to_delete=$(find $source_dir -name "*.log" -mtime +7)
-cat $files_to_delete
-echo "files to be deleted: $files_to_delete"
+# Find .log files older than 7 days
+files_to_delete=$(find "$source_dir" -name "*.log" -mtime +7)
 
-while read -r file_to_do
-do
-    echo "deleting $file_to_do"
-    rm -rf $file_to_do
-done <<< $files_to_delete
+# Check if any files exist before proceeding
+if [ -z "$files_to_delete" ]; then
+    echo "No log files older than 7 days found. Exiting..." &>> "$logfilename"
+    exit 0
+fi
+
+echo "Files to be deleted:" &>> "$logfilename"
+echo "$files_to_delete" &>> "$logfilename"
+
+# Safely delete files
+while IFS= read -r file_to_delete; do
+    echo "Deleting: $file_to_delete" &>> "$logfilename"
+    rm -f "$file_to_delete"
+done <<< "$files_to_delete"
+
+echo "Log cleanup completed successfully." &>> "$logfilename"
