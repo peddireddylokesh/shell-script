cc73f96aebcf86961a8ff3bf231bdac4715907e8
peddireddylokesh <peddireddylokesh999@gmail.com>
2025-02-14 10:44:03 +0530

    testing shellscript

--- disk-usage.sh
+++ disk-usage.sh
@@ -7,12 +7,12 @@ msg=""
 while read -r line
 do 
    
-    usage=$(echo $line | awk  -F " " '{print $6F}' | cut -d "%" -f1)
+    usage=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
     partition=$(echo $line | awk  -F " " '{print $NF}')
    # echo "partition  is $partition ,usage : $usage"
     if [ $usage -ge $disk_threshhold ]
     then
-        msg+="Disk usage is high --- $partition usage is --- $usage \n "
+        msg+="Disk usage is high: $partition usage is $usage /n "
     fi
 done <<< $disk_usage
 
